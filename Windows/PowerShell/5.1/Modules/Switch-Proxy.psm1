6190b8b025d2edc8770dfd4eee1f773c4b070e33
bilbobaggs <112827850+bilbobaggs@users.noreply.github.com>
2023-09-26 10:12:10 +0900

    Function to toggle the state of the proxy settings for PowerShell

---
+++ Windows/PowerShell/5.1/Modules/Switch-Proxy.psm1
@@ -0,0 +1,27 @@
+﻿function Switch-Proxy () {
+    <#
+    .SYNOPSIS
+        Switch-Proxy toggles proxy settings on and off.
+
+    .DESCRIPTION
+        Switch-Proxy will automatically detect if the state of the proxy and toggle it to the oppsite.
+
+    .INPUTS
+        None
+
+    .OUTPUTS
+        None
+
+    .NOTES
+        Use the Common Parameters at your own risk, I'm not sure if they will work.
+    #>
+
+    $proxyScript = 'https://pac.menlosecurity.com/af-5cf035477276/wpad.dat'
+    $currentValue = Get-ItemProperty -Path HKCU:"Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigURL -ErrorAction SilentlyContinue
+
+    if ([string]::IsNullOrEmpty($currentValue)) {
+        Set-ItemProperty -Path HKCU:"Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigURL -Value $proxyScript
+    } else {
+        Remove-ItemProperty -Path HKCU:"Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigURL
+    }
+}
